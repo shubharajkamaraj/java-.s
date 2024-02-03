3d3fbb09afc87ddd8eaca8f65fd26e4baed30f75
shubharajkamaraj <158665203+shubharajkamaraj@users.noreply.github.com>
2024-02-03 09:07:59 +0530

    Create PASITIVENEGATIVE.java

---
+++ PASITIVENEGATIVE.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Positivenegative 
+{
+public static void main(String args[])
+{
+int num;
+Scanner scin=new Scanner(System.in);
+System.out.println("Enter the byte value : ");
+num=scin.nextInt();
+if(num>0)
+{
+System.out.println("It is Positive");
+}
+else
+{
+System.out.println("It is Negative ");
+}
+
+}
+}
